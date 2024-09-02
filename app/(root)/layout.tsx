c9d2a3d9798c65d7dbcb790fc8224c71b5f76ec9
מיכל שלמה <micshlom@g.jct.ac.il>
2024-09-02 13:37:23 +0300

    implement beginning of sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,17 @@
+import Sidebar from "@/components/Sidebar";
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Michal', lastName: 'Shlomo' };
+
   return (
-    <main>
-        SIDEBAR
-        {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar />
+      {children}
     </main>
   );
 }
